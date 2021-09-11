d5d558ecd2c655c584145c8e58d00a81437e592d
chd <homenet12@naver.com>
2021-09-11 19:59:20 +0900

    프로젝트 커밋

---
+++ src/main/java/com/wv/blog/WvblogApplication.java
@@ -0,0 +1,13 @@
+package com.wv.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WvblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wv/blog/WvblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wv.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WvblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
